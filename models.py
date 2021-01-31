1deaa7cc370462f069336d337f55f5c43d57c7e0
Mike Kennedy <michael_l_kennedy@apple.com>
2021-01-31 14:56:14 -0800

    Initial submission

---
+++ models.py
@@ -0,0 +1,155 @@
+"""Represent models for near-Earth objects and their close approaches.
+
+The `NearEarthObject` class represents a near-Earth object. Each has a unique
+primary designation, an optional unique name, an optional diameter, and a flag
+for whether the object is potentially hazardous.
+
+The `CloseApproach` class represents a close approach to Earth by an NEO. Each
+has an approach datetime, a nominal approach distance, and a relative approach
+velocity.
+
+A `NearEarthObject` maintains a collection of its close approaches, and a
+`CloseApproach` maintains a reference to its NEO.
+
+The functions that construct these objects use information extracted from the
+data files from NASA, so these objects should be able to handle all of the
+quirks of the data set, such as missing names and unknown diameters.
+
+You'll edit this file in Task 1.
+"""
+from helpers import cd_to_datetime, datetime_to_str
+
+
+class NearEarthObject:
+    """A near-Earth object (NEO).
+
+    An NEO encapsulates semantic and physical parameters about the object, such
+    as its primary designation (required, unique), IAU name (optional), diameter
+    in kilometers (optional - sometimes unknown), and whether it's marked as
+    potentially hazardous to Earth.
+
+    A `NearEarthObject` also maintains a collection of its close approaches -
+    initialized to an empty collection, but eventually populated in the
+    `NEODatabase` constructor.
+    """
+    # TODO: How can you, and should you, change the arguments to this constructor?
+    # If you make changes, be sure to update the comments in this file.
+    def __init__(self, **info):
+        """Create a new `NearEarthObject`.
+
+        :param info: A dictionary of excess keyword arguments supplied to the constructor.
+        """
+        # TODO: Assign information from the arguments passed to the constructor
+        # onto attributes named `designation`, `name`, `diameter`, and `hazardous`.
+        # You should coerce these values to their appropriate data type and
+        # handle any edge cases, such as a empty name being represented by `None`
+        # and a missing diameter being represented by `float('nan')`.
+        self.designation = str(info['designation']) if info['designation'] else ''
+        self.name = str(info['name']) if info['name'] else None
+        self.diameter = float(info['diameter']) if info['diameter'] else float('nan')
+        self.hazardous = True if info['hazardous'] == 'Y' else False
+
+        # Create an empty initial collection of linked approaches.
+        self.approaches = []
+
+    @property
+    def fullname(self):
+        """Return a representation of the full name of this NEO."""
+        # TODO: Use self.designation and self.name to build a fullname for this object.
+        return f"{self.designation} ({self.name or 'N/A'})"
+
+    def __str__(self):
+        """Return `str(self)`."""
+        # TODO: Use this object's attributes to return a human-readable string representation.
+        # The project instructions include one possibility. Peek at the __repr__
+        # method for examples of advanced string formatting.
+        return f"{self.fullname} is a near Earth object with a diameter of {self.diameter:.3f} and {'is' if self.hazardous else 'is not'} hazardous."
+
+    def __repr__(self):
+        """Return `repr(self)`, a computer-readable string representation of this object."""
+        return (f"NearEarthObject(designation={self.designation!r}, name={self.name!r}, "
+                f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})")
+
+
+class CloseApproach:
+    """A close approach to Earth by an NEO.
+
+    A `CloseApproach` encapsulates information about the NEO's close approach to
+    Earth, such as the date and time (in UTC) of closest approach, the nominal
+    approach distance in astronomical units, and the relative approach velocity
+    in kilometers per second.
+
+    A `CloseApproach` also maintains a reference to its `NearEarthObject` -
+    initally, this information (the NEO's primary designation) is saved in a
+    private attribute, but the referenced NEO is eventually replaced in the
+    `NEODatabase` constructor.
+    """
+    # TODO: How can you, and should you, change the arguments to this constructor?
+    # If you make changes, be sure to update the comments in this file.
+    def __init__(self, **info):
+        """Create a new `CloseApproach`.
+
+        :param info: A dictionary of excess keyword arguments supplied to the constructor.
+        """
+        # TODO: Assign information from the arguments passed to the constructor
+        # onto attributes named `_designation`, `time`, `distance`, and `velocity`.
+        # You should coerce these values to their appropriate data type and handle any edge cases.
+        # The `cd_to_datetime` function will be useful.
+        self._designation = str(info['designation']) if info['designation'] else ''
+        self.time = cd_to_datetime(info['time']) if info['time'] else None  # TODO: Use the cd_to_datetime function for this attribute.
+        self.distance = float(info['distance']) if info['distance'] else 0.0
+        self.velocity = float(info['velocity']) if info['velocity'] else 0.0
+
+        # Create an attribute for the referenced NEO, originally None.
+        self.neo = None
+
+    @property
+    def fullname(self):
+        """Return a representation of the full name of this NEO."""
+        # TODO: Use self.designation and self.name to build a fullname for this object.
+        return f"{self._designation} ({self.neo.name or 'N/A'})"
+
+    def serialize(self):
+        result = {
+            'datetime_utc': datetime_to_str(self.time),
+            'distance_au': self.distance,
+            'velocity_km_s': self.velocity,
+            'neo': {
+                'designation': self.neo.designation,
+                'name': self.neo.name,
+                'diameter_km': self.neo.diameter,
+                'potentially_hazardous': self.neo.hazardous
+            },
+        }
+        return result
+
+    @property
+    def time_str(self):
+        """Return a formatted representation of this `CloseApproach`'s approach time.
+
+        The value in `self.time` should be a Python `datetime` object. While a
+        `datetime` object has a string representation, the default representation
+        includes seconds - significant figures that don't exist in our input
+        data set.
+
+        The `datetime_to_str` method converts a `datetime` object to a
+        formatted string that can be used in human-readable representations and
+        in serialization to CSV and JSON files.
+        """
+        # TODO: Use this object's `.time` attribute and the `datetime_to_str` function to
+        # build a formatted representation of the approach time.
+        # TODO: Use self.designation and self.name to build a fullname for this object.
+        # On 2020-01-01 12:30, '2020 FK (One REALLY BIG fake asteroid)' approaches Earth at a distance of 0.25 au and a velocity of 56.78 km/s.
+        return datetime_to_str(self.time)
+
+    def __str__(self):
+        """Return `str(self)`."""
+        # TODO: Use this object's attributes to return a human-readable string representation.
+        # The project instructions include one possibility. Peek at the __repr__
+        # method for examples of advanced string formatting.
+        return f"On {self.time_str}, '{self.fullname}' approaches Earth at a distance of {self.distance:.2f} and a velocity of {self.velocity:.2f} km/s."
+
+    def __repr__(self):
+        """Return `repr(self)`, a computer-readable string representation of this object."""
+        return (f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, "
+                f"velocity={self.velocity:.2f}, neo={self.neo!r})")
---
+++ write.py
@@ -0,0 +1,70 @@
+"""Write a stream of close approaches to CSV or to JSON.
+
+This module exports two functions: `write_to_csv` and `write_to_json`, each of
+which accept an `results` stream of close approaches and a path to which to
+write the data.
+
+These functions are invoked by the main module with the output of the `limit`
+function and the filename supplied by the user at the command line. The file's
+extension determines which of these functions is used.
+
+You'll edit this file in Part 4.
+"""
+import csv
+import json
+
+
+def write_to_csv(results, filename):
+    """Write an iterable of `CloseApproach` objects to a CSV file.
+
+    The precise output specification is in `README.md`. Roughly, each output row
+    corresponds to the information in a single close approach from the `results`
+    stream and its associated near-Earth object.
+
+    :param results: An iterable of `CloseApproach` objects.
+    :param filename: A Path-like object pointing to where the data should be saved.
+    """
+    fieldnames = {
+                'datetime_utc': 'String date object as UTC',
+                'distance_au': 'Distance',
+                'velocity_km_s': 'Velocity in km/s',
+                'designation': 'Designation',
+                'name': 'Name',
+                'diameter_km': 'Diameter in km',
+                'potentially_hazardous': 'Potentially hazardous',
+            }
+            
+    with open(filename, 'w') as outfile:
+        writer = csv.DictWriter(outfile, fieldnames.keys())
+        writer.writeheader()
+        for result in results:
+            cad_object = result.serialize()
+            row = {
+                'datetime_utc': cad_object['datetime_utc'],
+                'distance_au': cad_object['distance_au'],
+                'velocity_km_s': cad_object['velocity_km_s'],
+                'designation': cad_object['neo']['designation'],
+                'name': cad_object['neo']['name'] if cad_object['neo']['name'] else 'None',
+                'diameter_km': cad_object['neo']['diameter_km'],
+                'potentially_hazardous': 'True' if cad_object['neo']['potentially_hazardous'] else 'False',
+            }
+            writer.writerow(row)
+
+
+def write_to_json(results, filename):
+    """Write an iterable of `CloseApproach` objects to a JSON file.
+
+    The precise output specification is in `README.md`. Roughly, the output is a
+    list containing dictionaries, each mapping `CloseApproach` attributes to
+    their values and the 'neo' key mapping to a dictionary of the associated
+    NEO's attributes.
+
+    :param results: An iterable of `CloseApproach` objects.
+    :param filename: A Path-like object pointing to where the data should be saved.
+    """
+    results_output = []
+    for result in results:
+        results_output.append(result.serialize())
+    
+    with open(filename, 'w') as outfile:
+        json.dump(results_output, outfile, indent=2)
